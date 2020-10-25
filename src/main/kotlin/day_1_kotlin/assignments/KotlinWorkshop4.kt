2aa4eced24b332e41618033f36916778d14d2b28
Artur Badretdinov <artursletter@gmail.com>
2020-10-25 09:21:18 +0300

    Return back some changes

--- src/main/kotlin/day_1_kotlin/assignments/KotlinWorkshop4.kt
+++ src/main/kotlin/day_1_kotlin/assignments/KotlinWorkshop4.kt
@@ -2,47 +2,21 @@ package day_1_kotlin.assignments
 
 // Workshop #4 - classes, properties, constructor, visibility modifiers
 
-object SimplePhoneChecker {
+typealias CodeChecker = (String) -> Boolean
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val codeChecker: (String) -> Boolean = { code -> code == "1234" }
+class SimplePhoneVerificator(private val codeChecker: CodeChecker) {
 
-        // TODO 1: Create an instance of SimplePhoneVerificator
-        val phoneVerificator = SimplePhoneVerificator(codeChecker)
-
-        val isVerified = false
-        // TODO 2: add logic here
-        //  Use SimplePhoneVerificator.verify and SimplePhoneVerificator.setVerifiedProperty methods to verify your phone
-        //  You program should print "Code is verified!"
-
-        if (isVerified) {
-            println("Code is verified!")
-        } else {
-            println("Error: Code is not verified!")
-        }
-    }
-}
-
-
-// Don't touch this part:
-class Animal(private val codeChecker: (String) -> Boolean) {
+    private val consoleLogger: ConsoleLogger = ConsoleLogger()
 
     private var phone: String = ""
 
-    /**
-     * Checks if a [code] is valid:
-     * 1. Not empty
-     * 2. Longer than 3 symbols
-     * 3. [codeChecker] returns true
-     */
     fun verify(code: String): Boolean {
         if (phone.isEmpty()) {
-            println("Phone is empty")
+            consoleLogger.log("Phone is empty")
             return false
         }
         if (code.length < 4) {
-            println("Code is too short")
+            consoleLogger.log("Code is too short")
             return false
         }
         return codeChecker.invoke(code)
@@ -50,5 +24,31 @@ class Animal(private val codeChecker: (String) -> Boolean) {
 
     fun setVerifiedProperty(property: String) {
         phone = property
+    }
+}
+
+class ConsoleLogger {
+    fun log(message: String) {
+        println(message)
+    }
+}
+
+object SimplePhoneChecker {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val codeChecker: CodeChecker = { code -> code == "1234" }
+        val phoneVerificator = SimplePhoneVerificator(codeChecker)
+
+        // TODO: add logic here
+        // Use SimplePhoneVerificator.verify and SimplePhoneVerificator.setVerifiedProperty methods to verify your phone
+        // You program should print "Code is verified!"
+
+        // TODO: Uncomment
+        /*if (isVerified) {
+            println("Code is verified!")
+        } else {
+            println("Error: Code is not verified!")
+        }*/
     }
 }
--- src/main/kotlin/day_1_kotlin/assignments/KotlinWorkshop5.kt
+++ src/main/kotlin/day_1_kotlin/assignments/KotlinWorkshop5.kt
@@ -2,51 +2,47 @@ package day_1_kotlin.assignments
 
 // workshop #5 - inheritance, abstract class, interface
 
-
-// TODO Sergey: format
-
-interface CanSpeak {
+interface Verificator {
     fun verify(code: String): Boolean
 }
 
 abstract class AbstractVerificator : Verificator {
 
+    protected val consoleLogger: ConsoleLogger = ConsoleLogger()
+
     protected var property: String = ""
 
     fun setVerifiedProperty(property: String) {
-        this.property = property
+        // TODO: Add code here
     }
 }
 
-// EXAMPLE:
 // Class for demonstrate interface and abstract class using
 // Compare it with SimplePhoneVerificator from KotlinWorkshop4
 // Can we replace SimplePhoneVerificator with PhoneVerificator in KotlinWorkshop4?
-class PhoneVerificator(private val codeChecker: (String) -> Boolean) : AbstractVerificator() {
+class PhoneVerificator(private val codeChecker: CodeChecker) : AbstractVerificator() {
     override fun verify(code: String): Boolean {
         if (property.isEmpty()) {
-            println("Phone is empty")
+            consoleLogger.log("Phone is empty")
             return false
         }
         if (code.length < 4) {
-            println("Code is too short")
+            consoleLogger.log("Code is too short")
             return false
         }
         return codeChecker.invoke(code)
     }
 }
 
-// Work zone: create a class similar to the one above
-// TODO 1: Define GmailVerificator that extends AbstractVerificator
-class ... () {
 
-    private val gmailCodeChecker: (String) -> Boolean = { code -> code == "gmailCode" }
+class GmailVerificator : AbstractVerificator() {
+
+    private val gmailCodeChecker: CodeChecker = { code -> code == "gmailCode" }
 
     override fun verify(code: String): Boolean {
-        return false // TODO 2: change return value for correct tests execution
+        return false // TODO: change return value for correct tests execution
     }
 }
-
 
 object GmailChecker {
 
@@ -54,32 +50,32 @@ object GmailChecker {
     fun main(args: Array<String>) {
         val gmailVerificator = GmailVerificator()
 
-        positiveTest(gmailVerificator)  // Should print true
+        positiveTest(gmailVerificator) // Should print true
         negativeTest1(gmailVerificator) // Should print false
         negativeTest2(gmailVerificator) // Should print false
     }
 
-    fun positiveTest(gmailVerificator: GmailVerificator): Boolean {
+    fun positiveTest(gmailVerificator: GmailVerificator) {
         val email = "kotlinWorkshop5@gmail.com"
         gmailVerificator.setVerifiedProperty(email)
         val code = "gmailCode"
         val isVerified = gmailVerificator.verify(code)
-        return isVerified
+        println(isVerified)
     }
 
-    fun negativeTest1(gmailVerificator: GmailVerificator): Boolean {
+    fun negativeTest1(gmailVerificator: GmailVerificator) {
         val email = "kotlinWorkshop5@gmail.com"
         gmailVerificator.setVerifiedProperty(email)
         val code = "someCode"
         val isVerified = gmailVerificator.verify(code)
-        return isVerified
+        println(isVerified)
     }
 
-    fun negativeTest2(gmailVerificator: GmailVerificator): Boolean {
+    fun negativeTest2(gmailVerificator: GmailVerificator) {
         val email = "kotlinWorkshop5@yandex.com"
         gmailVerificator.setVerifiedProperty(email)
         val code = "gmailCode"
         val isVerified = gmailVerificator.verify(code)
-        return isVerified
+        println(isVerified)
     }
 }
