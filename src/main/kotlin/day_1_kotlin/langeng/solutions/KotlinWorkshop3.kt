ebd8d0b26ff17044c9f485405b499cffe7df30e5
Sergio <webanimal.group@gmail.com>
2020-10-26 10:14:10 +0300

    Workshops 3 hotfix.

--- src/main/kotlin/day_1_kotlin/langeng/solutions/KotlinWorkshop3.kt
+++ src/main/kotlin/day_1_kotlin/langeng/solutions/KotlinWorkshop3.kt
@@ -4,7 +4,7 @@ import java.util.*
 import java.util.logging.Logger
 import kotlin.random.Random
 
-// Workshop #3 - functions, filters, lambda, high-order function
+// Workshop #3 - functions, lambda, high-order function
 
 object KotlinWorkshop3 {
 
@@ -26,22 +26,22 @@ object KotlinWorkshop3 {
         /* Exercise area */
 
         var guessed = false
-        var index = 0
-        while (!guessed && index < capacity) {
+        var counter = 0
+        while (!guessed && counter < capacity) {
             print("Input a number in range 0..$nonNullUpperBound inclusive: ")
             // Storing user input in userInput variable
             // While running the program. Look at the console. Click inside the console window. Input a number.
             val userInput: Int = getNextInput(scanner, nonNullUpperBound)
-            guesses[index] = userInput
+            guesses[counter] = userInput
 
             // TODO 1: Uncomment. Declare playRound function: takes 2 integer arguments and returns a boolean.
             guessed = playRound(userInput, randomNumber)
 
-            index++
+            counter++
         }
 
         // TODO (bonus): Create analytics system for the game. Collect stats and print.
-        printGameStats(guesses, randomNumber)
+        printGameStats(guesses, counter, randomNumber)
     }
 
     // TODO 1: Uncomment. Declare playRound function: takes 2 integer arguments and returns a boolean.
@@ -72,9 +72,9 @@ object KotlinWorkshop3 {
     /* Exercise bonus area */
 
     // TODO (bonus): Create analytics system for the game. Collect stats and print.
-    private fun printGameStats(guesses: IntArray, randomNumber: Int) {
+    private fun printGameStats(guesses: IntArray, guessCounter: Int, randomNumber: Int) {
         // TODO 3: Uncomment. Print total guesses count.
-        printTotalCountOfArray(guesses)
+        printTotalCountOfGuesses(guessCounter)
 
         // TODO 4: Uncomment.
         //  Add high level function "countHigherGuesses" for printing higher elements from array.
@@ -94,8 +94,8 @@ object KotlinWorkshop3 {
 
     // TODO 3
     // Should print total guesses count.
-    private fun printTotalCountOfArray(guesses: IntArray) {
-        println("\nTotal count: ${guesses.size}\n")
+    private fun printTotalCountOfGuesses(guessCounter: Int) {
+        println("\nTotal count: $guessCounter\n")
     }
 
     // TODO 4
